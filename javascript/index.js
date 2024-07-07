ad761c611308aea67a1370d51e57da22408c90d5
leacorrina <aculeawhite@gmail.com>
2024-07-06 21:02:33 -0700

    updated date and time with moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,26 @@
-let amsterdamElement = document.querySelector("#amsterdam");
-let amsterdamDateElement = amsterdamElement.querySelector(".date");
-let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+function updateTime() {
+  //Amsterdam
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
+
+  amsterdamDateElement.innerHTML = amsterdamTime.format("dddd MMMM D, YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Lisbon");
+
+  lisbonDateElement.innerHTML = lisbonTime.format("dddd MMMM D, YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
